2449846b47bf232ce91b795509a5a28530728991
Alex Chaffee <alexch@gmail.com>
2018-06-21 09:58:05 -0700

    more extraction

--- guess.js
+++ guess.js
@@ -1,7 +1,15 @@
-'use strict'
+'use strict';
 
 function say(message) {
     console.log(message);
+}
+
+process.stdin.on('data', (chunk) => {
+    handleInput(chunk.toString());
+});
+
+function exit() {
+    process.exit();
 }
 
 say('I, the computer, will guess any whole integer number between 1 and 100.\n You can tell me H to say the number is higher than my guess.\n You can tell me L to say the number is lower than my guess.\n You can tell me I was correct by entering C');
@@ -11,30 +19,30 @@ let lastGuess = originalRandomNumber;
 let maxGuess = 100;
 let minGuess = 1;
 
-say('Is your number ' + originalRandomNumber + '?')
+function makeGuess(guess) {
+    say('Is your number ' + guess + '?');
+}
 
-process.stdin.on('data', (chunk) => {
-    handleInput(chunk.toString());
-});
+makeGuess(originalRandomNumber);
 
 function handleInput(input) {
-    let input = input.trim().toUpperCase();
+    input = input.trim().toUpperCase();
     say('user input: ' + input);
     if (input === 'H') {
         //handle higher guess
         minGuess = lastGuess;
         lastGuess = getRandomInt(minGuess, maxGuess);
-        say('Is your number ' + lastGuess + '?');
+        makeGuess(lastGuess)
     }
     else if (input == 'L') {
         //handle lower guess
         maxGuess = lastGuess;
         lastGuess = getRandomInt(minGuess, maxGuess);
-        say('Is your number ' + lastGuess + '?');
+        makeGuess(lastGuess)
     }
     else if (input == 'C') {
         say(`🎉 I know the answer! Your number is (${lastGuess}). 🎉`);
-        process.exit();
+        exit();
     }
     else {
         say('Invalid input, try again (valid input: L for lower, H for higher or C for correct)');
