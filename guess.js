39c0d67434004f56d1bf362021c0b555d4a2a54a
Alex Chaffee <alexch@gmail.com>
2018-06-21 11:07:13 -0700

    html with some buttons and functions

--- guess.js
+++ guess.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // NODE STUFF
 global.say = function(message) {
     console.log(message);
@@ -19,5 +18,3 @@ let guessCore = require('./guess-core.js')
 say('I, the computer, will guess any whole integer number between 1 and 100.\n You can tell me H to say the number is higher than my guess.\n You can tell me L to say the number is lower than my guess.\n You can tell me I was correct by entering C');
 
 guessCore.start();
-
-
