cd3c404d84203bddc5fd8ac7051edf47388b5f5f
Alex Chaffee <alexch@gmail.com>
2018-06-21 10:00:06 -0700

    node stuff / core stuff

--- guess.js
+++ guess.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// NODE STUFF
 function say(message) {
     console.log(message);
 }
@@ -14,28 +15,28 @@ function exit() {
 
 say('I, the computer, will guess any whole integer number between 1 and 100.\n You can tell me H to say the number is higher than my guess.\n You can tell me L to say the number is lower than my guess.\n You can tell me I was correct by entering C');
 
+
+// CORE STUFF
 let originalRandomNumber = getRandomInt(1, 100);
 let lastGuess = originalRandomNumber;
 let maxGuess = 100;
 let minGuess = 1;
 
+makeGuess(originalRandomNumber);
+
 function makeGuess(guess) {
     say('Is your number ' + guess + '?');
 }
-
-makeGuess(originalRandomNumber);
 
 function handleInput(input) {
     input = input.trim().toUpperCase();
     say('user input: ' + input);
     if (input === 'H') {
-        //handle higher guess
         minGuess = lastGuess;
         lastGuess = getRandomInt(minGuess, maxGuess);
         makeGuess(lastGuess)
     }
     else if (input == 'L') {
-        //handle lower guess
         maxGuess = lastGuess;
         lastGuess = getRandomInt(minGuess, maxGuess);
         makeGuess(lastGuess)
