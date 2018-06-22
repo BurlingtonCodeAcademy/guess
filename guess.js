6baa3adfd87a62cbe6d98938302acb1621ba1aaa
Joshua Burke <burke.joshua.james@gmail.com>
2018-06-22 15:08:07 -0400

    Add message and question sections, list guesses

--- guess.js
+++ guess.js
@@ -2,6 +2,9 @@
 
 let say = function (string) {
     console.log(string);
+    let output = document.querySelector('#output p');
+    console.log(output);
+    output.innerHTML += '<br>' + string + '<br>';
 }
 
 let originalRandomNumber = getRandomInt(1, 100);
@@ -9,15 +12,10 @@ let lastGuess = originalRandomNumber;
 let maxGuess = 100;
 let minGuess = 1;
 
-say('I, the computer, will guess any whole integer number between 1 and 100.\n You can tell me H to say the number is higher than my guess.\n You can tell me L to say the number is lower than my guess.\n You can tell me I was correct by entering C');
-
-say('Is your number ' + originalRandomNumber + '?');
 
 let handleInput = function (rawInput) {
 
     let input = rawInput.toString().trim().toUpperCase();
-
-    say('user input: ' + input)
 
     if (input === 'H') {
 
@@ -35,8 +33,7 @@ let handleInput = function (rawInput) {
 
     } else if (input == 'C') {
 
-        say(`🎉 I know the answer! Your number is (${lastGuess}). 🎉`)
-        process.exit()
+        say(`🎉 I know the answer! Your number is <b>${lastGuess}</b>. 🎉`)
 
     } else {
         say('Invalid input, try again (valid input: L for lower, H for higher or C for correct)')
